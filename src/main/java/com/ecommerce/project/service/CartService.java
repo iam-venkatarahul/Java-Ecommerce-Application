abce21d07b753e837268becda9f764655203ecff
iam-venkatarahul <chintakayalavvrahul@gmail.com>
2025-08-19 20:23:58 +0530

    updated

--- src/main/java/com/ecommerce/project/service/CartService.java
+++ src/main/java/com/ecommerce/project/service/CartService.java
@@ -1,5 +1,16 @@
 package com.ecommerce.project.service;
 
+import java.util.List;
+
+import com.ecommerce.project.payload.CartDTO;
+import com.ecommerce.project.payload.CartItemDTO;
+
 public interface CartService {
+
+    String createOrUpdateCartWithItems(List<CartItemDTO> cartItems);
+
+    CartDTO addProductToCart(Long productId, Integer quantity);
+
+    List<CartDTO> getAllCarts();
     
 }
